5961864bf4845b26b99d6a3f741ffb9c51f16569
Impcx <wilsongunawan09@gmail.com>
2021-11-09 23:16:01 +0700

    Update Tampilan, Admin, Fix Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,10 +22,7 @@ const Header = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light py-2 px-4 justify-content-between">
       <a class="navbar-brand" href="/">Bukapedia</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div>
         <ul class="navbar-nav">
           <li class="nav-item active">
             <Link to="/" className="nav-link link-dark px-2">
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -34,11 +34,11 @@ const ProductDetail = () => {
   };
 
   return (
-    <div>
+    <div className="p-5">
       {Object.keys(product).length === 0 ? (
         <div>...Loading</div>
       ) : (
-            <div className="p-5">
+            <div>
               <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col-auto d-none d-lg-block">
                   {/* <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg> */}
